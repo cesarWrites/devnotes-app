cb40377f045eb56ea34e7cb17267c59aa4b673f6
Sarah Mukuti <cesarwrites24@gmail.com>
2022-07-24 14:12:39 +0300

    fetch data about all teh tech tips

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+//import {  Route, Switch } from "react-router-dom";
 import './App.css';
+import TechTips from './components/TechTips';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      
       <h1>DevNotes</h1>
+      <TechTips />
     </div>
   );
 }
--- src/components/TechTips.js
+++ src/components/TechTips.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
 
 function TechTips(){
+    const[tips, setTips] = useState("")
+
+    useEffect(()=>{
+        getTipDetails();
+    }, [])
+    const getTipDetails =() =>{
+        axios
+        .get('http://localhost:3000/tips')
+        .then((res)=>{
+            console.log(res);
+            setTips(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+  
+    };
+
     return(
         <div className="tech-tips">
-            <h1>User</h1>
-            <p>content</p>
+            <div className="tips-container">
+                {tips.map((tip) => (
+                    <div className="tips-det" key = {tip.id}>
+                        <h3>{tip.user}</h3>
+                        <p>{tip.content}</p>
+                        <button>Like</button>
+                        </div>
+                ))}
+                </div>
         </div>
     )
 }
