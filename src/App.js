0148bfc2eaab0d28d960e01f0d1dbbd97d3a5976
Sarah Mukuti <cesarwrites24@gmail.com>
2022-07-26 01:46:46 +0300

    create routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React from 'react';
 import NavBar from './components/NavBar';
-//import {  Route, Switch } from "react-router-dom";
+import {  BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Books from './components/books';
 import News from './components/News';
 import NewTechTipForm from './components/NewTip';
 import './App.css';
+import Home from './components/Home';
 import TechTips from './components/TechTips';
 
 function App() {
   return (
     <div className="App">
       <h1>DevNotes</h1>
-      <TechTips />
-      <Books  />
-      <News />
-      <NewTechTipForm />
+    <Router>
+      <div>
+      <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <Switch>
+      <Route path="/tips">
+        <TechTips />
+      </Route>
+      <Route path="/books">
+        <Books />
+      </Route>
+      <Route path="/news">
+        <News />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
