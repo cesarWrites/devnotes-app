10d65b526ad83bbe952d14852578137e9a5aa0f8
Sarah Mukuti <cesarwrites24@gmail.com>
2022-07-26 12:54:20 +0300

    add teh router paths

--- src/App.js
+++ src/App.js
@@ -1,48 +1,29 @@
 import React from 'react';
-import NavBar from './components/NavBar';
-import {  BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Books from './components/books';
 import News from './components/News';
-import NewTechTipForm from './components/NewTip';
 import './App.css';
+import NavBar from './components/NavBar';
 import Home from './components/Home';
 import TechTips from './components/TechTips';
 
 function App() {
   return (
     <div className="App">
-      <h1>DevNotes</h1>
-    <Router>
-      <div>
-      <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+       <h1>DevNotes</h1>
+      <Router>
+        <NavBar />
+      <Routes>
+      <Route path="/"
+      element={ <Home/> } />
+      <Route path="tips" 
+      element={ <TechTips/> } />
+      <Route path="books" 
+      element={ <Books/>} />
 
-      <Switch>
-      <Route path="/tips">
-        <TechTips />
-      </Route>
-      <Route path="/books">
-        <Books />
-      </Route>
-      <Route path="/news">
-        <News />
-      </Route>
-      <Route path="/">
-        <Home />
-      </Route>
-      </Switch>
+      <Route path="news" 
+      element={ <News/> } />
+      </Routes>
       </Router>
     </div>
   );
