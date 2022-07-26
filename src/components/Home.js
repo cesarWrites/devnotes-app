4d15a3b631530e4c60b43ef07c58d501a863ced5
Sarah Mukuti <cesarwrites24@gmail.com>
2022-07-26 12:54:40 +0300

    add content for teh home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
+  const imgurl = "https://media.istockphoto.com/photos/young-woman-texting-on-smart-phone-picture-id527270693?b=1&k=20&m=527270693&s=170667a&w=0&h=6iBSeYlTixFLSF-zlmERg7D06sDimfyUScO4tyBaMeQ=";
   return (
-  <div>
-    <h1>Home Page</h1>
+  <div className="home-div">
+    <article>
+      <p>Get all the tech resources and tips to make your programming journey and understanding of etchnology a little more easier.</p>
+      <p>Share your most amazing tips with the other tech enthusiasts </p>
+      <img src={imgurl}/>
+  </article>
   </div>
   )
 }
