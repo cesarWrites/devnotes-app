6b51cca1d906fadc1831a092965bd85f259c95d6
Sarah Mukuti <cesarwrites24@gmail.com>
2022-07-31 12:39:05 +0300

    Updates

---
+++ precache-manifest.835088158c883421027bc99809f425c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "543014d5c4d6285a65bb",
+    "url": "/devnotes-app/static/css/main.915c9cea.chunk.css"
+  },
+  {
+    "revision": "543014d5c4d6285a65bb",
+    "url": "/devnotes-app/static/js/main.543014d5.chunk.js"
+  },
+  {
+    "revision": "4422e644433f4a61be16",
+    "url": "/devnotes-app/static/js/1.4422e644.chunk.js"
+  },
+  {
+    "revision": "af4cb5c87c35f7894ccf",
+    "url": "/devnotes-app/static/js/2.af4cb5c8.chunk.js"
+  },
+  {
+    "revision": "6e9af60ce607c95113ca",
+    "url": "/devnotes-app/static/js/runtime~main.6e9af60c.js"
+  },
+  {
+    "revision": "b35fbb3636ddcd01ad52ffa43af9f067",
+    "url": "/devnotes-app/index.html"
+  }
+];
